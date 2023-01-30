49a9603cede954fd060c0171720bf6b2819c6653
Renato <renatoperezc@gmail.com>
2023-01-30 09:47:08 -0600

    Refactor Contact component to use json data

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,8 +2,10 @@ import { Box, Heading, HStack, Icon, Link, Text } from "@chakra-ui/react";
 import { FaRegEnvelope, FaGithub } from "react-icons/fa";
 import { TbWorld, TbExternalLink } from "react-icons/tb";
 import { GoLocation } from "react-icons/go";
+import data from "@utils/data/renato-resume-data.json";
 
 const Contact = () => {
+  const { email, website, github, location } = data.profile.contact;
   return (
     <Box w="100%" p="2">
       <Heading as="h3" fontSize="24px" fontWeight="medium">
@@ -13,36 +15,30 @@ const Contact = () => {
         <HStack spacing="4">
           <Icon as={FaRegEnvelope} fill="blue.500" />
           <Link href="mailto:contact@renatoperez.dev" color="blue.700">
-            contact@renatoperez.dev
+            {email}
           </Link>
         </HStack>
         <HStack spacing="4">
           <Icon as={TbWorld} fill="blue.500" />
-          <Link href="https://www.renatoperez.dev" color="blue.700">
+          <Link href={website.href} color="blue.700">
             <HStack align="center">
-              <Text>www.renatoperez.dev</Text>
-              <Icon
-                style={{ marginLeft: "1px", marginTop: "-2px" }}
-                as={TbExternalLink}
-              />
+              <Text>{website.text}</Text>
+              <Icon style={{ marginLeft: "1px", marginTop: "-2px" }} as={TbExternalLink} />
             </HStack>
           </Link>
         </HStack>
         <HStack spacing="4">
           <Icon as={FaGithub} fill="blue.500" />
-          <Link href="https://github.com/renato1010" color="blue.700">
+          <Link href={github.href} color="blue.700">
             <HStack align="center">
-              <Text>/renato1010</Text>
-              <Icon
-                style={{ marginLeft: "1px", marginTop: "-2px" }}
-                as={TbExternalLink}
-              />
+              <Text>{github.text}</Text>
+              <Icon style={{ marginLeft: "1px", marginTop: "-2px" }} as={TbExternalLink} />
             </HStack>
           </Link>
         </HStack>
         <HStack spacing="4">
           <Icon as={GoLocation} fill="blue.500" />
-          <Link color="blue.700">Guatemala|US|world</Link>
+          <Link color="blue.700">{location}</Link>
         </HStack>
       </Box>
     </Box>
